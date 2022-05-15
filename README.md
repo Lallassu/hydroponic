365a1d567648fc483e3deae83dcb4ee44b91fdde
Magnus Persson <nergal@nergal.se>
2022-05-15 13:22:41 +0200

    Working system

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Hydroponic
 Hydroponic automation with ESP32 and sensors
 
+- Temperature sensors  (DS18B20)
+- 5V Relays
 
 # Installation
 Using platformio:
@@ -12,6 +14,51 @@ pio run -t upload
 Read the serial interface to get log output:
 ```
 pio device monitor -b 115200
+```
+
+# Home Assistant Integration
+
+Temperature sensors:
+```
+sensor:
+    - platform: mqtt
+      state_topic: 'hydroponic/temperature1'
+      name: 'Temperature1'
+    - platform: mqtt
+      state_topic: 'hydroponic/temperature2'
+      name: 'Temperature2'
+```
+
+Relay switches:
+```
+switch:
+  - platform: rest
+    name: "hydroponic_fan"
+    method: "post"
+    resource: http://192.168.1.205/fan
+    body_on: '{"active": "true"}'
+    body_off: '{"active": "false"}'
+    is_on_template: "{{ value_json.is_active }}"
+    headers:
+      Content-Type: application/json
+  - platform: rest
+    name: "hydroponic_cam"
+    method: "post"
+    resource: http://192.168.1.205/cam
+    body_on: '{"active": "true"}'
+    body_off: '{"active": "false"}'
+    is_on_template: "{{ value_json.is_active }}"
+    headers:
+      Content-Type: application/json
+  - platform: rest
+    name: "hydroponic_pump"
+    method: "post"
+    resource: http://192.168.1.205/pump
+    body_on: '{"active": "true"}'
+    body_off: '{"active": "false"}'
+    is_on_template: "{{ value_json.is_active }}"
+    headers:
+      Content-Type: application/json
 ```
 
 # License
--- src/hydro.ino
+++ src/hydro.ino
@@ -10,6 +10,8 @@
 // Webserver
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <AsyncJson.h>
 
 // Requires for temperature sensor DS18B20
 #include <OneWire.h>
@@ -29,16 +31,16 @@
 #define TEMPERATURE_2_PIN 5
 
 // WiFi details
-#define SSID "__test__"
-#define PASSWORD "__test__"
+#define SSID "__ssid__"
+#define PASSWORD "__ssid_pass__"
 
 // MQTT
 #define MQTT_HOST "192.168.1.4"
-#define MQTT_PORT 188
-#define MQTT_USER "__test__"
-#define MQTT_PASS "__test__"
+#define MQTT_PORT 1883
+#define MQTT_USER "__mqtt_user__"
+#define MQTT_PASS "__mqtt_pass__"
 
-// Temperature update on MQTT
+// Temperature update interval on MQTT
 #define TEMPERATURE_UPDATE_DELAY_MS 30000
 
 ///////////////////////////////////////////////////////////////
@@ -51,74 +53,111 @@ DallasTemperature sensor1(&oneWire1);
 DallasTemperature sensor2(&oneWire2);
 AsyncWebServer server(80);
 PubSubClient mqttClient(client);
-
-void(* resetFunc) (void) = 0; //declare reset function @ address 0
+StaticJsonDocument<250> jsonDocument;
+char buffer[250];
+void(* resetFunc) (void) = 0;
 
 ///////////////////////////////////////////////////////////////
 // Setup 
 ///////////////////////////////////////////////////////////////
 void setup() {
+    // initialize digital pin as an output.
+    Serial.begin(115200);
+    while (!Serial) { ; }
 
-  // initialize digital pin as an output.
-  Serial.begin(115200);
-  while (!Serial) { ; }
+    StartWIFI();
 
-  StartWIFI();
-  Serial.print("Using IP address:");
-  Serial.println(WiFi.localIP());
+    // Start the DS18B20 sensors
+    sensor1.begin();
+    sensor2.begin();
 
-  // Start the DS18B20 sensors
-  sensor1.begin();
-  sensor2.begin();
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
-  Serial.print(F("Setup..."));
+    pinMode(RELAY_FAN_PIN, OUTPUT);
+    pinMode(RELAY_CAM_PIN, OUTPUT);
+    pinMode(RELAY_PUMP_PIN, OUTPUT);
 
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    // The relay uses high = off (in my case)
+    digitalWrite(RELAY_PUMP_PIN, HIGH);
+    digitalWrite(RELAY_CAM_PIN, HIGH);
+    digitalWrite(RELAY_FAN_PIN, HIGH);
 
-  pinMode(RELAY_FAN_PIN, OUTPUT);
-  pinMode(RELAY_CAM_PIN, OUTPUT);
-  pinMode(RELAY_PUMP_PIN, OUTPUT);
+    // https://www.home-assistant.io/integrations/switch.rest/
 
-  // The relay uses high = off (in my case)
-  digitalWrite(RELAY_PUMP_PIN, HIGH);
-  digitalWrite(RELAY_CAM_PIN, HIGH);
-  digitalWrite(RELAY_FAN_PIN, HIGH);
+    // FAN
+    server.addHandler(new AsyncCallbackJsonWebHandler("/fan", [](AsyncWebServerRequest *request, JsonVariant &json) {
+            HandleRequest(json, request, RELAY_FAN_PIN);
+            request->send(200, "application/json", buffer);
+    }));
 
-  // Create our async handlers.
-  // https://www.home-assistant.io/integrations/switch.rest/
-  server.on("/fan/on", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOn(RELAY_FAN_PIN);
-    req->send(200);
-  });
+    server.on("/fan", HTTP_GET, [](AsyncWebServerRequest *req) {
+            PinStateToJSON(RELAY_FAN_PIN);
+            req->send(200, "application/json", buffer);
+    });
 
-  server.on("/fan/off", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOff(RELAY_FAN_PIN);
-    req->send(200);
-  });
+    // CAM
+    server.addHandler(new AsyncCallbackJsonWebHandler("/cam", [](AsyncWebServerRequest *request, JsonVariant &json) {
+            HandleRequest(json, request, RELAY_CAM_PIN);
+            request->send(200, "application/json", buffer);
+    }));
 
-  server.on("/cam/on", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOn(RELAY_CAM_PIN);
-    req->send(200);
-  });
+    server.on("/cam", HTTP_GET, [](AsyncWebServerRequest *req) {
+            PinStateToJSON(RELAY_CAM_PIN);
+            req->send(200, "application/json", buffer);
+    });
 
-  server.on("/cam/off", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOff(RELAY_CAM_PIN);
-    req->send(200);
-  });
+    // PUMP
+    server.addHandler(new AsyncCallbackJsonWebHandler("/pump", [](AsyncWebServerRequest *request, JsonVariant &json) {
+            HandleRequest(json, request, RELAY_PUMP_PIN);
+            request->send(200, "application/json", buffer);
+    }));
 
-  server.on("/pump/on", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOn(RELAY_PUMP_PIN);
-    req->send(200);
-  });
+    server.on("/pump", HTTP_GET, [](AsyncWebServerRequest *req) {
+            PinStateToJSON(RELAY_PUMP_PIN);
+            req->send(200, "application/json", buffer);
+    });
 
-  server.on("/pump/off", HTTP_GET, [](AsyncWebServerRequest *req) {
-    RelayOff(RELAY_PUMP_PIN);
-    req->send(200);
-  });
+    Serial.print("Starting webserver on: ");
+    Serial.println(WiFi.localIP());
 
-  Serial.println(F("Complete, starting webserver."));
-  // Start the webserver
-  server.begin();
+    server.begin();
+}
+
+///////////////////////////////////////////////////////////////
+// read state of a pin and write json response
+///////////////////////////////////////////////////////////////
+void PinStateToJSON(int pin) {
+    jsonDocument.clear();  
+
+    // Read the output register to get the last state of the pin
+    gpio_num_t p = (gpio_num_t)(pin & 0x1F);
+    if ((GPIO_REG_READ(GPIO_OUT_REG) >> p) & 1U) {
+        jsonDocument["is_active"] = "false";
+    } else {
+        jsonDocument["is_active"] = "true";
+    }
+    serializeJson(jsonDocument, buffer);
+}
+
+///////////////////////////////////////////////////////////////
+// Handle a REST request for on/off of relays
+///////////////////////////////////////////////////////////////
+void HandleRequest(JsonVariant &json, AsyncWebServerRequest *req, int type) {  
+    jsonDocument.clear();  
+    JsonObject jsonObj = json.as<JsonObject>();
+    void (*fptr)(int);
+
+    if (jsonObj["active"] == "true") {
+        fptr = &RelayOn;
+        jsonDocument["is_active"] = "true";
+    } else if (jsonObj["active"] == "false") {
+        fptr = &RelayOff;
+        jsonDocument["is_active"] = "false";
+    }
+
+    (*fptr)(type);
+
+    serializeJson(jsonDocument, buffer);
 }
 
 ///////////////////////////////////////////////////////////////
@@ -138,7 +177,7 @@ void loop() {
 
     temp = ReadTemperature(sensor2);
     dtostrf(temp, 1,2, tempStr);
-    mqttClient.publish("hydroponic/temperature1", tempStr);
+    mqttClient.publish("hydroponic/temperature2", tempStr);
 
     delay(TEMPERATURE_UPDATE_DELAY_MS);
 }
@@ -147,7 +186,6 @@ void loop() {
 // RelayOn to turn on a relay with given ID
 ///////////////////////////////////////////////////////////////
 void RelayOn(int r) {
-    Serial.printf("ON relay: %d\n", r);
     digitalWrite(r, LOW);
 }
 
@@ -155,7 +193,6 @@ void RelayOn(int r) {
 // RelayOnff to turn off a relay with given ID
 ///////////////////////////////////////////////////////////////
 void RelayOff(int r) {
-    Serial.printf("OFF relay: %d\n", r);
     digitalWrite(r, HIGH);
 }
 
@@ -164,12 +201,9 @@ void RelayOff(int r) {
 ///////////////////////////////////////////////////////////////
 void StartMQTT() {
     while (!mqttClient.connected()) {
-        Serial.print("Attempting MQTT connection...");
         // Attempt to connect
-        if (mqttClient.connect("hydroponic", MQTT_USER, MQTT_PASS)) {
-            Serial.println("connected");
-        } else {
-            Serial.print("failed, state:");
+        if (!mqttClient.connect("hydroponic", MQTT_USER, MQTT_PASS)) {
+            Serial.print("mqtt failed, state:");
             Serial.print(mqttClient.state());
             Serial.println(" retry in 5 seconds...");
             delay(5000);
@@ -181,34 +215,27 @@ void StartMQTT() {
 // StartWIFI starts up the wifi connection
 ///////////////////////////////////////////////////////////////
 void StartWIFI() {
-  Serial.println("Connecting to WIFI");
-  Serial.println("");
+    Serial.println("Connecting to WIFI");
+    Serial.println("");
 
-  WiFi.disconnect(false);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, PASSWORD);
+    WiFi.disconnect(false);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD);
 
-  unsigned long startMills = millis();
+    unsigned long startMills = millis();
 
-  while (WiFi.status() != WL_CONNECTED) {
-    unsigned long cMills = millis();
+    while (WiFi.status() != WL_CONNECTED) {
+        unsigned long cMills = millis();
 
-    delay(500);
+        delay(500);
 
-    if (millis() - startMills > 60000 * 3) {
-      Serial.println("Could not connect to WIFI. Aborting");
-      Serial.println("rebooting");
-      resetFunc();
-      return;
+        if (millis() - startMills > 60000 * 3) {
+            Serial.println("Could not connect to WIFI. Aborting");
+            Serial.println("rebooting");
+            resetFunc();
+            return;
+        }
     }
-  }
-
-  delay(1500);
-
-  IPAddress ip;
-  ip = WiFi.localIP();
-  Serial.println(F("WiFi connected"));
-  Serial.println(ip);
 }
 
 ///////////////////////////////////////////////////////////////
